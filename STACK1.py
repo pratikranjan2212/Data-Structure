43739330dbb59cfd011ca1c38919f44f2581d0af
Pratik Ranjan <128955674+pratikranjan2212@users.noreply.github.com>
2023-08-05 16:35:30 +0530

    Update STACK1.py

--- STACK1.py
+++ STACK1.py
@@ -50,9 +50,35 @@ while True:
     print("4.Display Stack")
     print("5.Exit")
     ch=int(input("Enter your choice(1-5):"))
-    if ch==1:
-        name=input("Enter your name:")
-        id=int(input("Enter id:"))
-        desig=input('Enter Designation:')
-        mark=int(input('Enter marks'))
-        item=[name]
+    if ch == 1:
+        name = input("Enter your name:")
+        id_ = int(input("Enter id:"))
+        desig = input('Enter Designation:')
+        mark = int(input('Enter marks'))
+        item = [id_, name, desig, mark]  # Create a list with the given information
+        Push(stk, item)  # Append the list to the stack
+        print("Item pushed to the stack successfully!")
+
+    elif ch == 2:
+        item = Pop(stk)
+        if item == 'Underflow':
+            print("Stack Underflow! Cannot pop from an empty stack.")
+        else:
+            print("Popped item:", item)
+
+    elif ch == 3:
+        item = Peek(stk)
+        if item == "Underflow":
+            print("Stack is Empty! Nothing to peek.")
+        else:
+            print("Top item:", item)
+
+    elif ch == 4:
+        Display(stk)
+
+    elif ch == 5:
+        print("Exiting...")
+        break
+
+    else:
+        print("Invalid choice. Please enter a valid option.")
